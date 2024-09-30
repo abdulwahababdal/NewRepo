c1c8d3e49e69f98d649e373b7160e8e0576eef90
Abdulwahab <abdulwahababdal@gmail.com>
2024-09-30 09:54:44 +0300

    CtoF

---
+++ main.dart
@@ -0,0 +1,8 @@
+
+
+void main(){
+  double tempInFahrenheit = 86;
+  double celsius = (tempInFahrenheit-32)/1.8;
+  print("$tempInFahrenheit F= ${(tempInFahrenheit-32)/1.8}C");
+  print("Celsius to Fahrenheit: ${celsius*1.8000+32.00}");
+}
